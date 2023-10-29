09dfcebb3ff5e36e5d0a8deb316ae8d19fee7a72
GabrielMS1903 <149096770+GabrielMS1903@users.noreply.github.com>
2023-10-28 21:46:28 -0300

    Update calculadora.js

---
+++ calculadora.js
@@ -0,0 +1,21 @@
+function ranked (victory, defeat){
+    return (victory - defeat)
+ 
+   
+}
+	let pontos = ranked (100,50)
+	console.log("Your final ponts in the ranked are: " + pontos)
+if (ranked = 50){ console.log ("Your position is: Silver. Congragulations!")
+  
+  } 
+    let prata = "Silver"
+if (prata = "Silver"){
+    console.log("You up!")
+}
+    let name = "Yian@Dio"  
+    let i = 0
+do {
+    console.log ( name + " has " + ranked + " pontos " + "and him finished in : " + prata )
+
+}while(i>3)
+// nesse projeto eu quis utilizar maneiras mais simples de fazer os códigos, focando na organização e sendo objetivo em um resultado.
